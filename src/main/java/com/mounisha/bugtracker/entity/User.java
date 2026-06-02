613661be12d4433cf1608c526f764881d67583bf
Mounisha025 <padala.mounisha.08@gmail.com>
2026-06-02 23:25:00 +0530

    Created User entity

---
+++ src/main/java/com/mounisha/bugtracker/entity/User.java
@@ -0,0 +1,31 @@
+package com.mounisha.bugtracker.entity;
+
+import com.mounisha.bugtracker.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
